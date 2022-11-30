5a042d9036fd1a96751a33759e1140df83a080c6
@reeeray <sheingrisha@mail.ru>
2022-11-30 23:22:38 +0100

    daily easy peasy data structures

---
+++ src/main/java/leetcode/arrays/UniqueNumberOfOccurances_1207.java
@@ -0,0 +1,33 @@
+package leetcode.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * User : Shein G.A.{@reeeray}
+ * Date : 30.11.2022
+ **/
+public class UniqueNumberOfOccurances_1207 {
+
+    public static void main(String[] args) {
+        final int[] input = {1,2,2,1,1,3};
+        assert Boolean.TRUE == uniqueOccurrences(input);
+    }
+
+    private static boolean uniqueOccurrences(final int[] arr) {
+        final Map<Integer, Integer> occurrences = new HashMap<>();
+        for(int val : arr) {
+            occurrences.put(val, occurrences.getOrDefault(val, 0) + 1);
+        }
+
+        final Set<Integer> uniq = new HashSet<>();
+        for(final int occ : occurrences.values()) {
+            if(!uniq.add(occ)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
