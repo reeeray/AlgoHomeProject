7238063a3293f018856c0d3553744e7a941938ce
@reeeray <sheingrisha@mail.ru>
2023-01-15 22:46:12 +0100

    graph used union find, too hard voor mij, just copied

--- src/main/java/leetcode/graphs/NumberOfGoodPaths_2421.java
+++ src/main/java/leetcode/graphs/NumberOfGoodPaths_2421.java
@@ -1,8 +1,133 @@
 package leetcode.graphs;
 
+import java.util.*;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 15.01.2023
  **/
 public class NumberOfGoodPaths_2421 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static int goodPaths = 0;
+
+    //Bruit force solution
+    //Gives Time Limited Exception
+    public static int numberOfGoodPathsTLE(int[] vals, int[][] edges) {
+        final Map<Integer, List<Integer>> graph = new HashMap<>();
+
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+
+            graph.computeIfAbsent(a, value -> new ArrayList<>()).add(b);
+            graph.computeIfAbsent(b, value -> new ArrayList<>()).add(a);
+        }
+
+        for (int key : graph.keySet()) {
+            boolean[] visited = new boolean[vals.length];
+            dfs(graph, vals, visited, key, key);
+        }
+
+        return goodPaths / 2 + vals.length;
+    }
+
+    private static void dfs(Map<Integer, List<Integer>> graph, int[] vals, boolean[] visited, int start, int curr) {
+        if (start != curr && vals[start] == vals[curr])
+            goodPaths++;
+
+        visited[curr] = true;
+
+        for (int child : graph.get(curr)) {
+            if (!visited[child] && vals[child] <= vals[start])
+                dfs(graph, vals, visited, start, child);
+        }
+    }
+
+    public int numberOfGoodPaths(int[] vals, int[][] edges) {
+        final Map<Integer, List<Integer>> adj = new HashMap<>();
+        for (int[] edge : edges) {
+            int a = edge[0], b = edge[1];
+            adj.computeIfAbsent(a, value -> new ArrayList<Integer>()).add(b);
+            adj.computeIfAbsent(b, value -> new ArrayList<Integer>()).add(a);
+        }
+
+        int n = vals.length;
+        // Mapping from value to all the nodes having the same value in sorted order of
+        // values.
+        final TreeMap<Integer, List<Integer>> valuesToNodes = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            valuesToNodes.computeIfAbsent(vals[i], value -> new ArrayList<Integer>()).add(i);
+        }
+
+        final UnionFind dsu = new UnionFind(n);
+        int goodPaths = 0;
+
+        // Iterate over all the nodes with the same value in sorted order, starting from
+        // the lowest value.
+        for (int value : valuesToNodes.keySet()) {
+            // For every node in nodes, combine the sets of the node and its neighbors into
+            // one set.
+            for (int node : valuesToNodes.get(value)) {
+                if (!adj.containsKey(node))
+                    continue;
+                for (int neighbor : adj.get(node)) {
+                    // Only choose neighbors with a smaller value, as there is no point in
+                    // traversing to other neighbors.
+                    if (vals[node] >= vals[neighbor]) {
+                        dsu.union_set(node, neighbor);
+                    }
+                }
+            }
+            // Map to compute the number of nodes under observation (with the same values)
+            // in each of the sets.
+            final Map<Integer, Integer> group = new HashMap<>();
+            // Iterate over all the nodes. Get the set of each node and increase the count
+            // of the set by 1.
+            for (int u : valuesToNodes.get(value)) {
+                group.put(dsu.find(u), group.getOrDefault(dsu.find(u), 0) + 1);
+            }
+            // For each set of "size", add size * (size + 1) / 2 to the number of goodPaths.
+            for (int key : group.keySet()) {
+                int size = group.get(key);
+                goodPaths += size * (size + 1) / 2;
+            }
+        }
+        return goodPaths;
+    }
+
+    private static class UnionFind {
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(int size) {
+            parent = new int[size];
+            for (int i = 0; i < size; i++)
+                parent[i] = i;
+            rank = new int[size];
+        }
+
+        public int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        public void union_set(int x, int y) {
+            int xset = find(x), yset = find(y);
+            if (xset == yset) {
+                return;
+            } else if (rank[xset] < rank[yset]) {
+                parent[xset] = yset;
+            } else if (rank[xset] > rank[yset]) {
+                parent[yset] = xset;
+            } else {
+                parent[yset] = xset;
+                rank[xset]++;
+            }
+        }
+    }
 }
