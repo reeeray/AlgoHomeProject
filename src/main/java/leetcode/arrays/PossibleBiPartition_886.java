e020cba810d3514f64b490633eca451038d7f7f5
@reeeray <sheingrisha@mail.ru>
2023-01-15 22:45:06 +0100

    graph used union find, too hard voor mij, just copied

---
+++ src/main/java/leetcode/arrays/PossibleBiPartition_886.java
@@ -0,0 +1,8 @@
+package leetcode.arrays;
+
+/**
+ * User : Shein G.A.{@reeeray}
+ * Date : 21.12.2022
+ **/
+public class PossibleBiPartition_886 {
+}
---
+++ src/main/java/leetcode/graphs/NumberOfGoodPaths_2421.java
@@ -0,0 +1,8 @@
+package leetcode.graphs;
+
+/**
+ * User : Shein G.A.{@reeeray}
+ * Date : 15.01.2023
+ **/
+public class NumberOfGoodPaths_2421 {
+}
