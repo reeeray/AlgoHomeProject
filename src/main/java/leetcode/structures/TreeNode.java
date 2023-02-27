76908df10bfa0a63992fc651a17aa5bcc1a77a31
@reeeray <sheingrisha@mail.ru>
2023-02-27 23:00:00 +0100

    LeetCode-104 : helper

--- src/main/java/leetcode/structures/TreeNode.java
+++ src/main/java/leetcode/structures/TreeNode.java
@@ -1,8 +1,24 @@
 package leetcode.structures;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 16.02.2023
  **/
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TreeNode {
+
+    private int val;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(final int val) {
+        this.val = val;
+    }
+
 }
