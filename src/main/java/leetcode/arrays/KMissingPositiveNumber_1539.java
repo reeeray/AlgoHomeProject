6cb4068679cbb8980b044ecf1e1cc661be416f29
@reeeray <sheingrisha@mail.ru>
2023-03-06 22:12:51 +0100

    LeetCode-1539 : easy peasy

--- src/main/java/leetcode/arrays/KMissingPositiveNumber_1539.java
+++ src/main/java/leetcode/arrays/KMissingPositiveNumber_1539.java
@@ -5,4 +5,25 @@ package leetcode.arrays;
  * Date : 06.03.2023
  **/
 public class KMissingPositiveNumber_1539 {
+
+    public static void main(String[] args) {
+        final int[] input = {1, 2, 3, 4};
+        assert 6 == findKthPositive(input, 2);
+    }
+
+    public static int findKthPositive(final int[] arr, final int k) {
+        int pointer = 0;
+        int missingCounter = 0;
+        for(int i=1; i<Integer.MAX_VALUE; i++) {
+            if(pointer < arr.length && arr[pointer] == i) {
+                pointer++;
+            } else {
+                missingCounter++;
+            }
+            if(missingCounter == k) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
