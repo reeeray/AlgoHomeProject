fd24676d9d1520db982baad2cef35b1761079c38
@reeeray <sheingrisha@mail.ru>
2023-05-11 21:29:18 +0200

    LeetCode-1035 : DP

--- src/main/java/leetcode/arrays/UncrossedLines_1035.java
+++ src/main/java/leetcode/arrays/UncrossedLines_1035.java
@@ -5,4 +5,27 @@ package leetcode.arrays;
  * Date : 11.05.2023
  **/
 public class UncrossedLines_1035 {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static int maxUncrossedLines(final int[] nums1, final int[] nums2) {
+        final int n1 = nums1.length;
+        final int n2 = nums2.length;
+        final int[][] dp = new int[n1+1][n2+1];
+        for(int i=0; i<n1; i++) {
+            for(int j=0; j<n2; j++) {
+                if(i==0 || j==0) {
+                    dp[i][j] = 0;
+                } else if(nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
 }
