195c4af3a45e0a2b38a7f0d4c0d9e6052eb885d5
@reeeray <sheingrisha@mail.ru>
2023-05-18 22:55:01 +0200

    LeetCode-1557 : tricky one

--- src/main/java/leetcode/collections/MinNumberOfVerticesToReachAllNodes_1557.java
+++ src/main/java/leetcode/collections/MinNumberOfVerticesToReachAllNodes_1557.java
@@ -1,8 +1,27 @@
 package leetcode.collections;
 
+import java.util.*;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 18.05.2023
  **/
 public class MinNumberOfVerticesToReachAllNodes_1557 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static List<Integer> findSmallestSetOfVertices(final int n, final List<List<Integer>> edges) {
+        final List<Integer> res = new ArrayList<>();
+        final int[] visited = new int[n];
+        for(final List<Integer> v : edges) {
+            visited[v.get(1)] = 1;
+        }
+
+        for(int i=0; i<n; i++) {
+            if(visited[i] == 0) res.add(i);
+        }
+        return res;
+    }
 }
