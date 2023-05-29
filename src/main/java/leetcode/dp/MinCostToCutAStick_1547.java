ce1166e5c02d25a91e05557b5ff3eea9298cf46b
@reeeray <sheingrisha@mail.ru>
2023-05-29 13:33:07 +0200

    LeetCode-1547 : update on my algo but it's not working on corner cases

--- src/main/java/leetcode/dp/MinCostToCutAStick_1547.java
+++ src/main/java/leetcode/dp/MinCostToCutAStick_1547.java
@@ -11,6 +11,10 @@ public class MinCostToCutAStick_1547 {
     private static int[][] dp;
 
     public static void main(String[] args) {
+//        final int[] cuts = {5, 6, 1, 4, 2};
+//        System.out.println(minCostEff(9, cuts));
+        final int[] cuts = {1, 3, 4, 5};
+        System.out.println(minCostEff(7, cuts));
 
     }
 
@@ -34,5 +38,42 @@ public class MinCostToCutAStick_1547 {
             cost = Math.min(cost, (stickIndexRight - stickIndexLeft) + left_cost + right_cost);
         }
         return dp[left][right] = cost;
+    }
+
+    //NOT WORKING!!!!!!!!!!!!!!Incorrect answer
+    private static int minCostEff(final int n, final int[] cuts) {
+        Arrays.sort(cuts);
+        return solveEff(0, n, cuts, 0 ,cuts.length);
+    }
+
+    private static int solveEff (final int leftBound, final int rightBound, final int[] cuts, final int left, final int right) {
+        if(right - left < 1) return 0;
+        int minDiff = Integer.MAX_VALUE;
+        int resCutIndex = 0;
+        int balanceDiff = Integer.MAX_VALUE;
+        for(int i=left; i<right; i++) {
+            int diff = Math.abs((cuts[i] - leftBound) - (rightBound - cuts[i]));
+            int bd = Math.abs( i - (cuts.length - 1 - i));
+            if(diff <= minDiff) {
+                if(diff == minDiff && bd > balanceDiff) {
+                    continue;
+                }
+                if(diff == minDiff && bd == balanceDiff) {
+                    int res1 = solveEff(leftBound, cuts[resCutIndex], cuts, left, resCutIndex)  + solveEff(cuts[resCutIndex], rightBound, cuts, resCutIndex + 1, cuts.length);
+                    int res2 = solveEff(leftBound, cuts[i], cuts, left, i)  + solveEff(cuts[i], rightBound, cuts, i + 1, cuts.length);
+
+                    if(res1 < res2) {
+                        continue;
+                    }
+                }
+                resCutIndex = i;
+                minDiff = diff;
+                balanceDiff = bd;
+            }
+        }
+
+       return (rightBound - leftBound)
+               + solveEff(leftBound, cuts[resCutIndex], cuts, left, resCutIndex)
+               + solveEff(cuts[resCutIndex], rightBound, cuts, resCutIndex + 1, right);
     }
 }
