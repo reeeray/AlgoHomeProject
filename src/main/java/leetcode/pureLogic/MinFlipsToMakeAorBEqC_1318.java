93a146f6c193aa2da9ad35a2d0f1435afc39a81b
@reeeray <sheingrisha@mail.ru>
2023-06-07 23:25:30 +0200

    LeetCode-1318 : bit operands

--- src/main/java/leetcode/pureLogic/MinFlipsToMakeAorBEqC_1318.java
+++ src/main/java/leetcode/pureLogic/MinFlipsToMakeAorBEqC_1318.java
@@ -5,4 +5,33 @@ package leetcode.pureLogic;
  * Date : 07.06.2023
  **/
 public class MinFlipsToMakeAorBEqC_1318 {
+
+    public static void main(String[] args) {
+        minFlips(2, 6, 5);
+    }
+
+
+    //check how works bit operands
+    public static int minFlips(int a, int b, int c) {
+        int flips = 0;
+        while (a > 0 || b > 0 || c > 0) {
+            int bitA = a & 1;
+            int bitB = b & 1;
+            int bitC = c & 1;
+
+            if (bitC == 0) {
+                flips += (bitA + bitB);
+            } else {
+                if (bitA == 0 && bitB == 0) {
+                    flips += 1;
+                }
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+
+        return flips;
+    }
 }
