571f480c03c2628b35f74f6863754a0e236d8edd
@reeeray <sheingrisha@mail.ru>
2023-06-12 23:32:11 +0200

    LeetCode-228 : Summary Ranges

--- src/main/java/leetcode/arrays/SummaryRanges_228.java
+++ src/main/java/leetcode/arrays/SummaryRanges_228.java
@@ -1,8 +1,35 @@
 package leetcode.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 12.06.2023
  **/
 public class SummaryRanges_228 {
+
+    public static void main(String[] args) {
+        System.out.println(summaryRanges(new int[] {1, 2, 3, 5, 6, 8}));
+    }
+
+    public static List<String> summaryRanges(final int[] sums) {
+        final List<String> ranges = new ArrayList<>();
+        if(sums.length == 0){
+            return ranges;
+        }
+        int left = sums[0];
+        int right = sums[0];
+        int index = 0;
+        while(++index < sums.length) {
+            if(sums[index-1] != sums[index] - 1) {
+                ranges.add("" + left + (right > left ? "->" + right : ""));
+                left = sums[index];
+            } else {
+                right = sums[index];
+            }
+        }
+        ranges.add("" + left + (right > left ? "->" + right : ""));
+        return ranges;
+    }
 }
