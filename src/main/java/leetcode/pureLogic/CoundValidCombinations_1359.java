32edd7511964143b990a7a02b10f65a4a66a0b18
@reeeray <sheingrisha@mail.ru>
2023-09-10 17:20:52 +0200

    LeetCode-1359 : pretty hard

--- src/main/java/leetcode/pureLogic/CoundValidCombinations_1359.java
+++ src/main/java/leetcode/pureLogic/CoundValidCombinations_1359.java
@@ -5,4 +5,33 @@ package leetcode.pureLogic;
  * Date : 10.09.2023
  **/
 public class CoundValidCombinations_1359 {
+
+    private static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) {
+        assert countOrdersMath(8) == countOrders(8);
+    }
+
+    public static int countOrders(final int n) {
+        if(n == 1) {
+            return 1;
+        }
+        final long count = (1l * countOrders(n-1) * (2 * n - 1) * n) % MOD;
+        return (int)count;
+    }
+
+    //Combinatorial math
+    //C = (2*n)! / (2^n)
+    public static int countOrdersMath(final int n) {
+        long numerator = 1;
+
+        for(int i=2; i<=2*n; i++) {
+            numerator *= i;
+        }
+
+        final double devider = Math.pow(2, n);
+
+        final double answer = numerator / devider;
+        return (int)(answer % MOD);
+    }
 }
