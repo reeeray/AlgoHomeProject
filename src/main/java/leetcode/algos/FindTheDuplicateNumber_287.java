d71b42844355be849c29a2d158fd6903a50b2f22
@reeeray <sheingrisha@mail.ru>
2023-09-19 22:19:59 +0200

    LeetCode-287 : Floyd's algo

--- src/main/java/leetcode/algos/FindTheDuplicateNumber_287.java
+++ src/main/java/leetcode/algos/FindTheDuplicateNumber_287.java
@@ -1,8 +1,31 @@
 package leetcode.algos;
 
 /**
+ *
+ * Known problem. Resplution is Floyd's algorithm
  * User : Shein G.A.{@reeeray}
  * Date : 19.09.2023
  **/
 public class FindTheDuplicateNumber_287 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int findDuplicates(final int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
 }
