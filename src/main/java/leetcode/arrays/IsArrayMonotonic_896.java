36fbd52794ac5b9f641d091d6fb476ae5d2c16b2
@reeeray <sheingrisha@mail.ru>
2023-09-29 21:40:17 +0200

    LeetCode-896 : very basics

--- src/main/java/leetcode/arrays/IsArrayMonotonic_896.java
+++ src/main/java/leetcode/arrays/IsArrayMonotonic_896.java
@@ -5,4 +5,24 @@ package leetcode.arrays;
  * Date : 29.09.2023
  **/
 public class IsArrayMonotonic_896 {
+
+
+    public static boolean isMonotonic(final int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false;
+            } else if (nums[i] < nums[i - 1]) {
+                increasing = false;
+            }
+
+            if (!increasing && !decreasing) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
