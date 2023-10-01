15203a7cca6ff9d4948128a41828d7eac9e70fa3
@reeeray <sheingrisha@mail.ru>
2023-10-01 22:11:38 +0200

    LeetCode-557 : reverse order of words in a sentence

--- src/main/java/leetcode/strings/ReverseOrderString_557.java
+++ src/main/java/leetcode/strings/ReverseOrderString_557.java
@@ -5,4 +5,29 @@ package leetcode.strings;
  * Date : 01.10.2023
  **/
 public class ReverseOrderString_557 {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static String reverseWords(String s) {
+        int lastSpaceIndex = -1;
+        final char[] chArray = s.toCharArray();
+        int len = s.length();
+        for (int strIndex = 0; strIndex <= len; strIndex++) {
+            if (strIndex == len || chArray[strIndex] == ' ') {
+                int startIndex = lastSpaceIndex + 1;
+                int endIndex = strIndex - 1;
+                while (startIndex < endIndex) {
+                    char temp = chArray[startIndex];
+                    chArray[startIndex] = chArray[endIndex];
+                    chArray[endIndex] = temp;
+                    startIndex++;
+                    endIndex--;
+                }
+                lastSpaceIndex = strIndex;
+            }
+        }
+        return new String(chArray);
+    }
 }
