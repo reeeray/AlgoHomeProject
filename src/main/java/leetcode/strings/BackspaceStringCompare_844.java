5b577e71a5d549de8577b95f6d84dc3c7429d1f0
@reeeray <sheingrisha@mail.ru>
2023-10-19 22:11:47 +0200

    LeetCode-844 : a bit manipulation of structures or logic

--- src/main/java/leetcode/strings/BackspaceStringCompare_844.java
+++ src/main/java/leetcode/strings/BackspaceStringCompare_844.java
@@ -5,4 +5,49 @@ package leetcode.strings;
  * Date : 19.10.2023
  **/
 public class BackspaceStringCompare_844 {
+
+    public static void main(String[] args) {
+        final String s = "nzp#o#g";
+        final String t = "b#nzp#o#g";
+        backspaceCompare(s, t);
+    }
+
+    public static boolean backspaceCompare(final String s, final String t) {
+        int left = s.length() - 1;
+        int right = t.length() - 1;
+
+        while(left >= 0 || right >= 0) {
+            left = findNextIndex(s, left);
+            right = findNextIndex(t, right);
+            if((left >= 0 && right < 0) || (right >= 0 && left < 0)) {
+                return false;
+            }
+            if(left >= 0 && right >= 0 && s.charAt(left) != t.charAt(right)) {
+                return false;
+            }
+            left --;
+            right--;
+        }
+        return true;
+    }
+
+    private static int findNextIndex(final String s, final int index) {
+        if(index < 0 || s.charAt(index) != '#') {
+            return index;
+        }
+
+        int toSkip = 0;
+        for(int i=index; i>= 0; i--) {
+            if(s.charAt(i) == '#') {
+                toSkip++;
+                continue;
+            }else if(toSkip > 0) {
+                toSkip--;
+                continue;
+            } else {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
