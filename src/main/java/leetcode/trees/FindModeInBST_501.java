c350679970895c9c8d6c6e0ead4abb6e7d1454c7
@reeeray <sheingrisha@mail.ru>
2023-11-01 23:26:28 +0100

    LeetCode-501 : non optimized dfs

--- src/main/java/leetcode/trees/FindModeInBST_501.java
+++ src/main/java/leetcode/trees/FindModeInBST_501.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class FindModeInBST_501 {
 
     public static void main(String[] args) {
-        final TreeNode
+
     }
 
     public static int[] findModel(final TreeNode root) {
