ff232490e5a9c3496a34a9b4d9892f3376794bdd
@reeeray <sheingrisha@mail.ru>
2023-11-01 23:26:03 +0100

    LeetCode-501 : non optimized dfs

---
+++ src/main/java/leetcode/trees/FindModeInBST_501.java
@@ -0,0 +1,45 @@
+package leetcode.trees;
+
+import leetcode.structures.TreeNode;
+
+import java.util.*;
+
+/**
+ * User : Shein G.A.{@reeeray}
+ * Date : 01.11.2023
+ **/
+public class FindModeInBST_501 {
+
+    public static void main(String[] args) {
+        final TreeNode
+    }
+
+    public static int[] findModel(final TreeNode root) {
+        if (root == null) {
+            return new int[]{};
+        }
+        final Map<Integer, Integer> hash = new HashMap<>();
+        dfs(root, hash);
+        final List<Integer> list = new ArrayList<>();
+        int mode = -1;
+        for(final int count : hash.values()) {
+            mode = Math.max(count, mode);
+        }
+        for(final int count : hash.keySet()) {
+            if (hash.get(count) == mode) {
+                list.add(count);
+            }
+        }
+
+        return list.stream().mapToInt(v -> v).toArray();
+    }
+
+    private static void dfs(final TreeNode curr, final Map<Integer, Integer> hash) {
+        if(curr == null) {
+            return;
+        }
+        hash.put(curr.getVal(), hash.getOrDefault(curr.getVal(), 0) + 1);
+        dfs(curr.left, hash);
+        dfs(curr.right, hash);
+    }
+}
