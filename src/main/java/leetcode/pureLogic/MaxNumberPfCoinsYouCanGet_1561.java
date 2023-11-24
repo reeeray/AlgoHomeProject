36febf6384da00133034a4f868b560441d2f80b0
@reeeray <sheingrisha@mail.ru>
2023-11-24 23:15:03 +0100

    LeetCode-1561 : easy peasy logic

--- src/main/java/leetcode/pureLogic/MaxNumberPfCoinsYouCanGet_1561.java
+++ src/main/java/leetcode/pureLogic/MaxNumberPfCoinsYouCanGet_1561.java
@@ -1,8 +1,27 @@
 package leetcode.pureLogic;
 
+import java.util.Arrays;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 24.11.2023
  **/
 public class MaxNumberPfCoinsYouCanGet_1561 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxCoins(final int[] piles) {
+        Arrays.sort(piles);
+        int n = piles.length / 3;
+        int total = 0;
+        int index = piles.length - 2;
+        while(n > 0) {
+            total += piles[index];
+            index -= 2;
+            n--;
+        }
+        return total;
+    }
 }
