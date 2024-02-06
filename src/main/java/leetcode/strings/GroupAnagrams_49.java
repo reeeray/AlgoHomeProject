e30e186e6c1e6a6fdf25303a58906280b2bb1f6c
@reeeray <sheingrisha@mail.ru>
2024-02-06 21:19:04 +0100

    LeetCode-49 : updated existing solution

--- src/main/java/leetcode/strings/GroupAnagrams_49.java
+++ src/main/java/leetcode/strings/GroupAnagrams_49.java
@@ -10,7 +10,7 @@ public class GroupAnagrams_49 {
 
     public static void main(String[] args) {
         final String[] input = {"eat","tea","tan","ate","nat","bat"};
-        final List<List<String>> answer = groupAnagrams(input);
+        final List<List<String>> answer = groupAnagramsWorser(input);
         System.out.println(answer);
         assert  answer.size() == 3;
 
@@ -34,5 +34,28 @@ public class GroupAnagrams_49 {
         final char[] charArr = str.toLowerCase().toCharArray();
         Arrays.sort(charArr);
         return new String(charArr);
+    }
+
+    private static List<List<String>> groupAnagramsWorser(final String[] strs) {
+        final Map<String, List<String>> anagrams = new HashMap<>();
+        for(final String word : strs) {
+            final int[] chars = new int[26];
+            for(char c : word.toCharArray())
+                chars[c - 'a']++;
+            final String key = Arrays.toString(chars);
+            anagrams.computeIfAbsent(key, v -> new ArrayList<>()).add(word);
+        }
+        return new ArrayList<>(anagrams.values());
+    }
+
+    private static List<List<String>> groupAnagramsElegant(final String[] strs) {
+        final Map<String, List<String>> anagrams = new HashMap<>();
+        for(final String word : strs) {
+            final char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            final String key = Arrays.toString(chars);
+            anagrams.computeIfAbsent(key, v -> new ArrayList<>()).add(word);
+        }
+        return new ArrayList<>(anagrams.values());
     }
 }
