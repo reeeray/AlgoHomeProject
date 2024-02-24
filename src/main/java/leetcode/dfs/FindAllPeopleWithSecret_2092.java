46da3f22b56d6f9ff232528596654e8d0e2382d0
@reeeray <sheingrisha@mail.ru>
2024-02-24 20:48:22 +0100

    LeetCode-2092 : complicated task, possible optimizations is through the union find

---
+++ src/main/java/leetcode/dfs/FindAllPeopleWithSecret_2092.java
@@ -0,0 +1,73 @@
+package leetcode.dfs;
+
+import java.util.*;
+
+/**
+ * User : Shein G.A.{@reeeray}
+ * Date : 24.02.2024
+ **/
+public class FindAllPeopleWithSecret_2092 {
+
+    //Time O(M * log(M+N)) and Space O(M + N) where N -number of people and M - number of meetings
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        // Sort meetings in increasing order of time
+        Arrays.sort(meetings, (a, b) -> a[2] - b[2]);
+
+        // Group Meetings in increasing order of time
+        final Map<Integer, List<int[]>> sameTimeMeetings = new TreeMap<>();
+        for (int[] meeting : meetings) {
+            int x = meeting[0], y = meeting[1], t = meeting[2];
+            sameTimeMeetings.computeIfAbsent(t, k -> new ArrayList<>()).add(new int[]{x, y});
+        }
+
+        // Boolean Array to mark if a person knows the secret or not
+        final boolean[] knowsSecret = new boolean[n];
+        knowsSecret[0] = true;
+        knowsSecret[firstPerson] = true;
+
+        // Process in increasing order of time
+        for (int t : sameTimeMeetings.keySet()) {
+            // For each person, save all the people whom he/she meets at time t
+            final Map<Integer, List<Integer>> meet = new HashMap<>();
+            for (int[] meeting : sameTimeMeetings.get(t)) {
+                int x = meeting[0], y = meeting[1];
+                meet.computeIfAbsent(x, k -> new ArrayList<>()).add(y);
+                meet.computeIfAbsent(y, k -> new ArrayList<>()).add(x);
+            }
+
+            // Start traversal from those who already know the secret at time t
+            // Set to avoid redundancy
+            final Set<Integer> start = new HashSet<>();
+            for (int[] meeting : sameTimeMeetings.get(t)) {
+                int x = meeting[0], y = meeting[1];
+                if (knowsSecret[x]) {
+                    start.add(x);
+                }
+                if (knowsSecret[y]) {
+                    start.add(y);
+                }
+            }
+
+            // Do BFS
+            final Queue<Integer> q = new LinkedList<>(start);
+            while (!q.isEmpty()) {
+                int person = q.poll();
+                for (int nextPerson : meet.getOrDefault(person, new ArrayList<>())) {
+                    if (!knowsSecret[nextPerson]) {
+                        knowsSecret[nextPerson] = true;
+                        q.offer(nextPerson);
+                    }
+                }
+            }
+        }
+
+        // List of people who know the secret
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            if (knowsSecret[i]) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
