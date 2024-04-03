4719812d30c7d4d13670e55df3875c90d4891202
@reeeray <sheingrisha@mail.ru>
2024-04-03 22:17:32 +0200

    LeetCode-79 : updated with dfs

--- src/main/java/leetcode/arrays/WordSearch_79.java
+++ src/main/java/leetcode/arrays/WordSearch_79.java
@@ -8,9 +8,9 @@ public class WordSearch_79 {
 
     public static void main(String[] args) {
         final char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-        final String word = "ABCB";
+        final String word = "SEE";
         System.out.println(word.substring(1));
-        exist(board, word);
+        existOptimized(board, word);
     }
 
     private static boolean exist(final char[][] board, final String word) {
@@ -58,7 +58,7 @@ public class WordSearch_79 {
         return false;
     }
 
-    private boolean dfs(final int row, final int col, final int i, final boolean[][] visited, final char[][] board, final String word){
+    private static boolean dfs(final int row, final int col, final int i, final char[][] board, final String word){
         if(i == word.length()){
             return true;
         }
@@ -68,26 +68,28 @@ public class WordSearch_79 {
         if(word.charAt(i) != board[row][col]){
             return false;
         }
-        if(visited[row][col] ){
+        if(board[row][col] == 0){
             return false;
         }
-        visited[row][col] = true;
-        boolean res = dfs(row + 1, col, i + 1, visited, board, word);
-        res = res || dfs(row - 1, col, i + 1, visited, board, word);
-        res = res || dfs(row, col + 1, i + 1, visited, board, word);
-        res = res || dfs(col, col - 1, i + 1, visited, board, word);
+        final char c = board[row][col];
+        board[row][col] = 0;
+        boolean res = dfs(row + 1, col, i + 1, board, word);
+        res = res || dfs(row - 1, col, i + 1, board, word);
+        res = res || dfs(row, col + 1, i + 1, board, word);
+        res = res || dfs(row, col - 1, i + 1, board, word);
 
+        board[row][col] = c;
         return res;
     }
-//    public boolean exist(char[][] board, String word) {
-//        for(int i = 0; i < board.length; i++){
-//            for(int j = 0; j < board[0].length; j++){
-//                boolean[][] visited = new boolean[board.length][board[0].length];
-//                if(dfs(i, j, 0, visited, board, word)){
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
+
+    public static boolean existOptimized(final char[][] board, final String word) {
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(dfs(i, j, 0, board, word)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
