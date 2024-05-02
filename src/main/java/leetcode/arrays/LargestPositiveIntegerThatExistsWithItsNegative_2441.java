1b69d5ccc423518e8f2eb252b535ed9113fdad51
@reeeray <sheingrisha@mail.ru>
2024-05-02 21:30:51 +0200

    LeetCode-2441 : could be improved with bit operations

--- src/main/java/leetcode/arrays/LargestPositiveIntegerThatExistsWithItsNegative_2441.java
+++ src/main/java/leetcode/arrays/LargestPositiveIntegerThatExistsWithItsNegative_2441.java
@@ -1,8 +1,27 @@
 package leetcode.arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 02.05.2024
  **/
 public class LargestPositiveIntegerThatExistsWithItsNegative_2441 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int finadMaxK(final int[] nums) {
+        final Set<Integer> numbers = new HashSet<>();
+        int max = -1;
+        for(final int num : nums) {
+            if(numbers.contains(-num)) {
+               max = Math.max(max, Math.abs(num));
+            }
+            numbers.add(num);
+        }
+        return max;
+    }
 }
