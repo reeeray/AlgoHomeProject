9f47e1ade4e0ac9ffec8b25fd220b370cf97f758
@reeeray <sheingrisha@mail.ru>
2024-05-06 22:13:15 +0200

    LeetCode-2487 : linked list exercise

--- src/main/java/leetcode/collections/RemoveNodesFromLinkedList_2487.java
+++ src/main/java/leetcode/collections/RemoveNodesFromLinkedList_2487.java
@@ -1,8 +1,37 @@
 package leetcode.collections;
 
+import leetcode.structures.ListNode;
+
+import java.util.Stack;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 06.05.2024
  **/
 public class RemoveNodesFromLinkedList_2487 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode removeNodes(final ListNode head) {
+        final Stack<ListNode> iterator = new Stack<>();
+        ListNode node = head;
+        while (node != null) {
+            iterator.add(node);
+            node = node.next;
+        }
+        node = iterator.pop();
+        int max = node.val;
+
+        while(!iterator.isEmpty()) {
+            ListNode curr = iterator.pop();
+            if(curr.val >= max) {
+                max = curr.val;
+                curr.next = node;
+                node = curr;
+            }
+        }
+        return node;
+    }
 }
