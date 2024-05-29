57d1c108dd8683ccf98331a665d0d93500772f06
@reeeray <sheingrisha@mail.ru>
2024-05-29 22:50:47 +0200

    LeetCode-1404 : more logic than binary

--- src/main/java/leetcode/binaryStuff/NumberOfStepsToReduceANumberInBinaryReprToOne_1404.java
+++ src/main/java/leetcode/binaryStuff/NumberOfStepsToReduceANumberInBinaryReprToOne_1404.java
@@ -5,4 +5,29 @@ package leetcode.binaryStuff;
  * Date : 29.05.2024
  **/
 public class NumberOfStepsToReduceANumberInBinaryReprToOne_1404 {
+
+    public static void main(String[] args) {
+        numSteps("1101");
+    }
+
+    public static int numSteps(final String s) {
+        int total = 0;
+        int numberOfOnes = 0;
+        for(int i=s.length()-1; i>0; i--) {
+            boolean isOne = s.charAt(i) == '1';
+            if(numberOfOnes != 0) {
+                isOne = !isOne;
+                if(isOne) {
+                    numberOfOnes--;
+                }
+            }
+            if(isOne) {
+                total+=2;
+                numberOfOnes++;
+            } else {
+                total++;
+            }
+        }
+        return numberOfOnes == 0 ? total : total + 1;
+    }
 }
