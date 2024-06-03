141b9a029cdfb2332380d10afb25fac82d635e4b
@reeeray <sheingrisha@mail.ru>
2024-06-03 19:29:43 +0200

    LeetCode-2486 : weird task, definetely easier than medium

--- src/main/java/leetcode/dfs/AppendCharactersToStringToMakeSubsequence_2486.java
+++ src/main/java/leetcode/dfs/AppendCharactersToStringToMakeSubsequence_2486.java
@@ -5,4 +5,23 @@ package leetcode.dfs;
  * Date : 03.06.2024
  **/
 public class AppendCharactersToStringToMakeSubsequence_2486 {
+
+    static int max = 0;
+
+    public static void main(String[] args) {
+        appendCharacters("coaching", "coding");
+    }
+
+    public static int appendCharacters(final String s, final String t) {
+        int indexT = 0;
+        for(final char c : s.toCharArray()) {
+            if(c == t.charAt(indexT)) {
+                indexT++;
+            }
+            if(indexT == t.length()) {
+                return 0;
+            }
+        }
+        return t.length() - indexT;
+    }
 }
