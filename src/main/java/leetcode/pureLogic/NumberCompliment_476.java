05026a9f6d06d299d1ff651260f9674656657ee2
@reeeray <sheingrisha@mail.ru>
2024-08-22 21:11:50 +0200

    LeetCode-476 : swap all bits

--- src/main/java/leetcode/pureLogic/NumberCompliment_476.java
+++ src/main/java/leetcode/pureLogic/NumberCompliment_476.java
@@ -5,4 +5,27 @@ package leetcode.pureLogic;
  * Date : 22.08.2024
  **/
 public class NumberCompliment_476 {
+
+    public static void main(String[] args) {
+        findCompliment(5);
+    }
+
+    public static int findCompliment(final int num) {
+        final char[] representation = Integer.toBinaryString(num).toCharArray();
+        for(int i=0; i<representation.length; i++) {
+            representation[i] = representation[i] == '0' ? '1' : '0';
+        }
+        return Integer.parseInt(new String(representation), 2);
+    }
+
+    //Time O(1) and Space O(1)
+    public int findComplementBitManipulation(int num) {
+        if (num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+
+        int mask = (1 << bitLength) - 1;
+
+        return num ^ mask;
+    }
 }
