3809c775ab2ba299a747de53b02750b78d8e9fbd
@reeeray <sheingrisha@mail.ru>
2024-09-28 09:30:57 +0200

    LeetCode-struct : node structure implementation

--- src/main/java/leetcode/structures/Node.java
+++ src/main/java/leetcode/structures/Node.java
@@ -5,4 +5,15 @@ package leetcode.structures;
  * Date : 28.09.2024
  **/
 public class Node {
+
+    int val;
+    Node prev;
+    Node next;
+
+    public Node(final int val, final Node prev, final Node next) {
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+
 }
