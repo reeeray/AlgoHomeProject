2322c5edf8c1fa4b06e91a1481d9a05c71290027
@reeeray <sheingrisha@mail.ru>
2024-09-28 09:31:40 +0200

    LeetCode-641 : circular deque with operation based on linkedlist implementation

--- src/main/java/leetcode/structures/DesignCircularDeque_641.java
+++ src/main/java/leetcode/structures/DesignCircularDeque_641.java
@@ -5,4 +5,89 @@ package leetcode.structures;
  * Date : 28.09.2024
  **/
 public class DesignCircularDeque_641 {
+
+    Node head;
+    Node rear;
+    int size;
+    int capacity;
+
+    public DesignCircularDeque_641(final int k) {
+        size = 0;
+        capacity = k;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean insertFront(final int value) {
+        if (isFull()) return false;
+        if (head == null) {
+            rear = head = new Node(value, null, null);
+        } else {
+            // add new head
+            head.prev = new Node(value, null, head);
+            head = head.prev;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean insertLast(int value) {
+        if (isFull()) return false;
+        if (head == null) {
+            // first element in list
+            head = new Node(value, null, null);
+            rear = head;
+        } else {
+            // add new element to end
+            rear.next = new Node(value, rear, null);
+            rear = rear.next;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean deleteFront() {
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = null;
+            rear = null;
+        } else {
+            head = head.next;
+        }
+        size--;
+        return true;
+    }
+
+    public boolean deleteLast() {
+        if (isEmpty()) return false;
+        if (size == 1) {
+            head = null;
+            rear = null;
+        } else {
+            // update rear to the previous node
+            rear = rear.prev;
+        }
+        size--;
+        return true;
+    }
+
+    public int getFront() {
+        if (isEmpty()) return -1;
+        return head.val;
+    }
+
+    public int getRear() {
+        if (isEmpty()) return -1;
+        return rear.val;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
 }
