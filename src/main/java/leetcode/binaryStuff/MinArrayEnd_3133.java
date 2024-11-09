4d3fd1419a23e7bdc1fa2b8e9bafc354790c1894
@reeeray <sheingrisha@mail.ru>
2024-11-09 14:47:16 +0100

    LeetCode-2275 : weak at bit masking and manipulations otherwise can be improved

--- src/main/java/leetcode/binaryStuff/MinArrayEnd_3133.java
+++ src/main/java/leetcode/binaryStuff/MinArrayEnd_3133.java
@@ -5,4 +5,17 @@ package leetcode.binaryStuff;
  * Date : 09.11.2024
  **/
 public class MinArrayEnd_3133 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Time O(n) and Space O(1)
+    public static long minEnd(int n, final int x) {
+        long res = x;
+        while (--n > 0) {
+            res = (res + 1) | x;
+        }
+        return res;
+    }
 }
