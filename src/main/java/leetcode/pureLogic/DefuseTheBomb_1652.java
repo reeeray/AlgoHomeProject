d7084940de3939f74867bc0aad4e3b1e5a8269c9
@reeeray <sheingrisha@mail.ru>
2024-11-18 21:12:26 +0100

    LeetCode-1652 : simple sliding window

--- src/main/java/leetcode/pureLogic/DefuseTheBomb_1652.java
+++ src/main/java/leetcode/pureLogic/DefuseTheBomb_1652.java
@@ -5,4 +5,44 @@ package leetcode.pureLogic;
  * Date : 18.11.2024
  **/
 public class DefuseTheBomb_1652 {
+
+    public static void main(String[] args) {
+        final int[] input1 = new int[] {5, 7, 1, 4};
+        final int[] input2 = new int[] {2, 4, 9, 3};
+        //decrypt(input1, 3);
+        decrypt(input2, -2);
+    }
+
+    //sliding window, Time O(n) and Time O(n)
+    public static int[] decrypt(final int[] code, final int k) {
+        if(k == 0) {
+            return new int[code.length];
+        }
+        int start, end, sum = 0;
+        if(k > 0) {
+            start = 1;
+            end = k;
+        } else {
+            start = code.length + k;
+            end = code.length - 1;
+        }
+        for(int i=start; i<=end; i++) {
+            sum += code[i];
+        }
+        final int[] ans = new int[code.length];
+        for(int i=0; i<code.length; i++) {
+            ans[i] = sum;
+            sum -= code[start];
+            start++;
+            end++;
+            if(start == code.length) {
+                start = 0;
+            }
+            if(end == code.length) {
+                end = 0;
+            }
+            sum += code[end];
+        }
+        return ans;
+    }
 }
