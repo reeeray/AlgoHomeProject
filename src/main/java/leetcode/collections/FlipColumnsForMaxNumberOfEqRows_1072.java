de100085ae1c8b01cbfd4bea1b80b8f006f9e968
@reeeray <sheingrisha@mail.ru>
2024-11-22 19:24:35 +0100

    LeetCode-2257 : check most often pattern

--- src/main/java/leetcode/collections/FlipColumnsForMaxNumberOfEqRows_1072.java
+++ src/main/java/leetcode/collections/FlipColumnsForMaxNumberOfEqRows_1072.java
@@ -1,8 +1,32 @@
 package leetcode.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 22.11.2024
  **/
 public class FlipColumnsForMaxNumberOfEqRows_1072 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int maxEqRowsAfterFlips(final int[][] matrix) {
+        final Map<String, Integer> patternFreqMap = new HashMap<>();
+        for(final int[] row : matrix) {
+            final StringBuilder sb = new StringBuilder();
+            for(int i=0; i<row.length; i++) {
+                if(row[0] == row[i]) {
+                    sb.append('a');
+                } else {
+                    sb.append('b');
+                }
+            }
+            final String str = sb.toString();
+            patternFreqMap.put(str, patternFreqMap.getOrDefault(str, 0) + 1);
+        }
+        return patternFreqMap.values().stream().max((Integer::compareTo)).get();
+    }
 }
