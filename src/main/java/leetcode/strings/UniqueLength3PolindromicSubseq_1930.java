81e895b4116eedfcc95da9841f5a497e162afc0e
@reeeray <sheingrisha@mail.ru>
2025-01-04 11:34:43 +0300

    LeetCode-1930 : updated methods and improved alogs

--- src/main/java/leetcode/strings/UniqueLength3PolindromicSubseq_1930.java
+++ src/main/java/leetcode/strings/UniqueLength3PolindromicSubseq_1930.java
@@ -14,6 +14,67 @@ public class UniqueLength3PolindromicSubseq_1930 {
         System.out.println(countPalindromicSubsequence("bbcbaba"));
     }
 
+
+    //Time O(n) Space O(1)
+    public static int countPalindromicSubsequence_1930(final String s) {
+        final int[] firstOccurance = new int[26];
+        Arrays.fill(firstOccurance, -1);
+        final int[] secondOccurance = new int[26];
+        for(int i=0; i<s.length(); i++) {
+            final int charIndex = s.charAt(i) - 'a';
+            if(firstOccurance[charIndex] == -1) {
+                firstOccurance[charIndex] = i;
+            }
+            secondOccurance [charIndex] = i;
+        }
+
+        int count = 0;
+        for(int i=0; i<26; i++) {
+            if(firstOccurance[i] == -1) {
+                continue;
+            }
+            final HashSet<Character> unique = new HashSet<>();
+            for(int j = firstOccurance[i] + 1; j <secondOccurance[i]; j++) {
+                unique.add(s.charAt(j));
+            }
+            count += unique.size();
+        }
+        return count;
+    }
+
+    // Time O(n) and Space O(1)
+    public int countPalindromicSubsequenceCountLettresInBetween(final String s) {
+        final Set<Character> letters = new HashSet();
+        for (final Character c : s.toCharArray()) {
+            letters.add(c);
+        }
+
+        int ans = 0;
+        for (final Character letter : letters) {
+            int i = -1;
+            int j = 0;
+
+            for (int k = 0; k < s.length(); k++) {
+                if (s.charAt(k) == letter) {
+                    if (i == -1) {
+                        i = k;
+                    }
+
+                    j = k;
+                }
+            }
+
+            final Set<Character> between = new HashSet();
+            for (int k = i + 1; k < j; k++) {
+                between.add(s.charAt(k));
+            }
+
+            ans += between.size();
+        }
+
+        return ans;
+    }
+
     //TLE
     public static int countPalindromicSubsequence(final String s) {
         final char[] chars = s.toCharArray();
@@ -32,32 +93,5 @@ public class UniqueLength3PolindromicSubseq_1930 {
             }
         }
         return unique.size();
-    }
-
-    public static int countWithoutTLE(final String s) {
-        final int[] firstindex = new int[26];
-        final int[] lastIndex = new int[26];
-        Arrays.fill(firstindex, -1);
-        for(int i = 0; i<s.length(); i++) {
-            final int index = s.charAt(i) - 'a';
-            if(firstindex[index] == -1) {
-                firstindex[index] = i;
-            }
-            lastIndex[index] = i;
-        }
-
-        int total = 0;
-        for(int i = 0; i < 26; i++) {
-            if(firstindex[i] == - 1) {
-                continue;
-            }
-
-            final Set<Character> uniqie = new HashSet<>();
-            for(int j=firstindex[i] + 1; j<lastIndex[i]; j++) {
-                uniqie.add(s.charAt(j));
-            }
-            total += uniqie.size();
-        }
-        return total;
     }
 }
