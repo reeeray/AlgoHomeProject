a491798c14a7846b27a432d7b31f050f63cc30fd
@reeeray <sheingrisha@mail.ru>
2025-01-11 10:17:41 +0100

    LeetCode-1400 : logical statement that it's enough to consider just odd part

--- src/main/java/leetcode/strings/ConstructKPalindromeStrings_1400.java
+++ src/main/java/leetcode/strings/ConstructKPalindromeStrings_1400.java
@@ -5,4 +5,23 @@ package leetcode.strings;
  * Date : 11.01.2025
  **/
 public class ConstructKPalindromeStrings_1400 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Time O(n) and Space O(1)
+    public static boolean canConstruct(final String s, final int k) {
+        if(s.length() < k) return false;
+        if(s.length() == k) return true;
+        final int[] chars = new int[26];
+        for(final char c : s.toCharArray()) {
+            chars[c - 'a']++;
+        }
+        int oddCount = 0;
+        for(int i=0; i<26; i++) {
+            oddCount += chars[i] % 2;
+        }
+        return oddCount <= k;
+    }
 }
