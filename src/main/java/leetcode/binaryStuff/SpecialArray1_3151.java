d5da560e0f2041d0611d6035b038174cda2fc7d7
@reeeray <sheingrisha@mail.ru>
2025-02-01 22:06:30 +0100

    LeetCode-3151 : simple binary operation

--- src/main/java/leetcode/binaryStuff/SpecialArray1_3151.java
+++ src/main/java/leetcode/binaryStuff/SpecialArray1_3151.java
@@ -5,4 +5,26 @@ package leetcode.binaryStuff;
  * Date : 01.02.2025
  **/
 public class SpecialArray1_3151 {
+
+    public static void main(String[] args) {
+        isArraySpecial(new int[] {4, 3, 1, 4});
+    }
+
+    public static boolean isArraySpecial(final int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            if(((nums[i - 1] & 1) ^ (nums[i] & 1)) == 0)  {
+                return false;
+            }
+        }
+        return true;
+    }
+    //Time O(n) and Space O(1)
+    public static boolean isArraySpecialNonOptimized(final int[] nums) {
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i - 1] % 2 == nums[i] % 2) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
