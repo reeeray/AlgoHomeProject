ef8b23ed4db5b38311cc4490216bef3a8f4aff28
@reeeray <sheingrisha@mail.ru>
2025-04-11 18:58:53 +0200

    LeetCode-2843 : logico

--- src/main/java/leetcode/pureLogic/CountSymmetricIntegers_2843.java
+++ src/main/java/leetcode/pureLogic/CountSymmetricIntegers_2843.java
@@ -5,4 +5,25 @@ package leetcode.pureLogic;
  * Date : 11.04.2025
  **/
 public class CountSymmetricIntegers_2843 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Time O(high - low) and Space O(1)
+    public static int countSymmetricIntegers(final int low, final int high) {
+        int count = 0;
+        for (int num = low; num <= high; num++) {
+            if (num < 100 && num % 11 == 0) {
+                count++;
+            } else if (1000 <= num && num < 10000) {
+                int left = num / 1000 + (num % 1000) / 100;
+                int right = (num % 100) / 10 + (num % 10);
+                if (left == right) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
