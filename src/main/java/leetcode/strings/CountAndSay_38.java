14fc77d813496b4bbfce77d3f406a6e343baf062
@reeeray <sheingrisha@mail.ru>
2025-04-18 16:05:54 +0200

    LeetCode-38 : typical task for stringbuilder, could be solved recursively

--- src/main/java/leetcode/strings/CountAndSay_38.java
+++ src/main/java/leetcode/strings/CountAndSay_38.java
@@ -11,6 +11,26 @@ public class CountAndSay_38 {
         assert countAndSay(4).equals("1211");
     }
 
+    //Time O(2^n) and Space O(2^n)
+    private static String countAndSayElegant(final int n) {
+        String res = "1";
+        for(int i = 1; i < n; i++) {
+            final StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for(int j = 1; j < res.length(); j++) {
+                if(res.charAt(j) == res.charAt(j - 1)) {
+                    count++;
+                } else {
+                    sb.append(count).append(res.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            sb.append(count).append(res.charAt(res.length() - 1));
+            res = sb.toString();
+        }
+        return res;
+    }
+
     private static String countAndSay(int n) {
         if(n == 0)
             return "";
