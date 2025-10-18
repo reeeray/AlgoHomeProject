374168d419f958bf612cc06e7d7c22da83575a45
@reeeray <sheingrisha@mail.ru>
2025-10-18 19:41:39 +0200

    LeetCode-3397 : sorting and greedy

--- src/main/java/leetcode/pureLogic/MaxNumberOfDistinctElementsAfterOperations_3397.java
+++ src/main/java/leetcode/pureLogic/MaxNumberOfDistinctElementsAfterOperations_3397.java
@@ -1,8 +1,28 @@
 package leetcode.pureLogic;
 
+import java.util.Arrays;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 18.10.2025
  **/
 public class MaxNumberOfDistinctElementsAfterOperations_3397 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //Space O(logn) for sorting and Time O(nlogn)
+    public static int maxDistinctElements(final int[] nums, final int k) {
+        int counter = 0, prev = Integer.MIN_VALUE;
+        Arrays.sort(nums);
+        for(int i = 0;  i < nums.length; i++) {
+            final int curr = Math.min(Math.max(nums[i] - k, prev + 1), nums[i] + k);
+            if(curr > prev) {
+                counter++;
+                prev = curr;
+            }
+        }
+        return counter;
+    }
 }
