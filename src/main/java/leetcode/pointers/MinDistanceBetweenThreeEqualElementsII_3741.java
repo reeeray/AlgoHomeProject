bc48096af371a700d51edeea52a848128cb1a130
@reeeray <sheingrisha@mail.ru>
2026-04-11 19:57:47 +0200

    LeetCode-3741 : traversal + hashing

--- src/main/java/leetcode/pointers/MinDistanceBetweenThreeEqualElementsII_3741.java
+++ src/main/java/leetcode/pointers/MinDistanceBetweenThreeEqualElementsII_3741.java
@@ -15,6 +15,27 @@ public class MinDistanceBetweenThreeEqualElementsII_3741 {
     }
 
     public static int minimumDistance(final int[] nums) {
-  37
+        final int n = nums.length;
+        final int[] nextNum = new int[n];
+        final Map<Integer, Integer> occurrences = new HashMap<>();
+        Arrays.fill(nextNum, -1);
+        for(int i = n - 1; i >= 0; i--) {
+            if(occurrences.get(nums[i]) != null) {
+                nextNum[i] = occurrences.get(nums[i]);
+            }
+            occurrences.put(nums[i], i);
+        }
+
+        int res = n + 1;
+        for(int i = 0; i < n; i++) {
+            final int secondPos = nextNum[i];
+            if(secondPos != -1) {
+                final int thirdPos = nextNum[secondPos];
+                if(thirdPos != -1) {
+                    res = Math.min(res, thirdPos - i);
+                }
+            }
+        }
+        return res == n + 1 ? -1 : res * 2;
     }
 }
