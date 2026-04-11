bc7c83ce1d03d7c326a07200a8e98fa3268525bd
@reeeray <sheingrisha@mail.ru>
2026-04-11 19:57:37 +0200

    LeetCode-3741 : traversal + hashing

--- src/main/java/leetcode/pointers/MinDistanceBetweenThreeEqualElementsII_3741.java
+++ src/main/java/leetcode/pointers/MinDistanceBetweenThreeEqualElementsII_3741.java
@@ -1,8 +1,20 @@
 package leetcode.pointers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User : Shein G.A.{@reeeray}
  * Date : 11.04.2026
  **/
 public class MinDistanceBetweenThreeEqualElementsII_3741 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int minimumDistance(final int[] nums) {
+  37
+    }
 }
